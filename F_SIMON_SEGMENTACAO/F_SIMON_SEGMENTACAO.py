f6b05ee6e0e4b070f56b0a4443984e6f07e3aefa
Yan Lucas Franca Arcanjo <lucas.arcanjo@grendene.com.br>
2024-10-28 17:07:31 -0300

    Primeiro commit

---
+++ F_SIMON_SEGMENTACAO/F_SIMON_SEGMENTACAO.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.providers.microsoft.mssql.operators.mssql import MsSqlOperator
+import datetime 
+
+default_args = {
+    'owner': 'yan arcanjo',
+    'start_date': datetime(2024, 1, 1),
+}
+
+with DAG(
+    'example_postgres_dag', 
+    default_args=default_args, 
+    schedule_interval='@daily',
+    catchup=False
+) as dag:
+    run_query = MsSqlOperator(
+        task_id='run_sql',
+        mssql_conn_id='db_engenharia',
+        sql="SELECT 1;"
+    )
+
+
+    run_query
