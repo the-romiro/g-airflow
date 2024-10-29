25de0879901e8266cb58fabd55607441e1e505c0
Yan Lucas Franca Arcanjo <lucas.arcanjo@grendene.com.br>
2024-10-29 10:19:57 -0300

    atualizar query

--- F_SIMON_SEGMENTACAO/sql_files/insert_DBO.FSEGMENTACAO_SIMON.sql
+++ F_SIMON_SEGMENTACAO/sql_files/insert_DBO.FSEGMENTACAO_SIMON.sql
@@ -1,5 +1,5 @@
 MERGE INTO dbo.fSegmentacao_Simon AS TARGET
-USING (SELECT ? as data, ? as equipamento, ? as segmentacao, ? as updated_at) AS SOURCE 
+USING (SELECT :data, :equipamento, :segmentacao, :updated_at) AS SOURCE 
 ON target.equipuipamento = source.equipamento AND target.data = source.data
 WHEN MATCHED THEN
     UPDATE SET 
