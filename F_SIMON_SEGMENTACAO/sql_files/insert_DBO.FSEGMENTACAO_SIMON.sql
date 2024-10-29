6286964ae08b5eec91829bd08f15cb87052b1fa8
Yan Lucas Franca Arcanjo <lucas.arcanjo@grendene.com.br>
2024-10-29 10:15:01 -0300

    fix sql file

--- F_SIMON_SEGMENTACAO/sql_files/insert_DBO.FSEGMENTACAO_SIMON.sql
+++ F_SIMON_SEGMENTACAO/sql_files/insert_DBO.FSEGMENTACAO_SIMON.sql
@@ -1,9 +1,10 @@
 MERGE INTO dbo.fSegmentacao_Simon AS TARGET
-USING (VALUES(?, ?, ?, ?)) AS SOURCE (data, equipamento, segmentacao, updated_at)
+USING (SELECT ? as data, ? as equipamento, ? as segmentacao, ? as updated_at) AS SOURCE 
 ON target.equipuipamento = source.equipamento AND target.data = source.data
 WHEN MATCHED THEN
-    UPDATE SET target.segmentacao = source.segmentacao
-    UPDATE SET target.updated_at = source.updated_at
+    UPDATE SET 
+        target.segmentacao = source.segmentacao, 
+        target.updated_at = source.updated_at   
 WHEN NOT MATCHED THEN
     INSERT (data, equipamento, segmentacao, updated_at)
     VALUES(source.data, source.equipamento, source.segmentacao, source.updated_at)
