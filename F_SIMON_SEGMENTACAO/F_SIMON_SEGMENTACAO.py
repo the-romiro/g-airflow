25a8bcb302e3aae50cb047c0ae64d7755d04716e
Yan Lucas Franca Arcanjo <lucas.arcanjo@grendene.com.br>
2024-10-29 09:57:40 -0300

    add population

--- F_SIMON_SEGMENTACAO/F_SIMON_SEGMENTACAO.py
+++ F_SIMON_SEGMENTACAO/F_SIMON_SEGMENTACAO.py
@@ -5,12 +5,12 @@ from datetime import datetime
 import pandas as pd
 import os
 
-
 default_args = {
     'owner': 'yan arcanjo',
     'start_date': datetime(2024, 1, 1, 7, 0),
 }
 
+#reads the sql file and returns the query
 def read_sql_file(file_path):
     dir = os.path.dirname(os.path.abspath(__file__))
     sql_dir = os.path.join(dir, file_path)
@@ -19,13 +19,22 @@ def read_sql_file(file_path):
         query = file.read()
     return query
 
+#Get the machine tables and returns a dataframe
 def get_machine_registration_table(**kwargs):
     hook = MsSqlHook(mssql_conn_id='db_engenharia')
-
-
     df = pd.read_sql_query(read_sql_file('sql_files/DBO.SIM_EQUIPAMENTOS.SQL'), hook.get_sqlalchemy_engine())
 
-    print(df)
+    return df
+
+def populate_fSegmentacao_simon(**kwargs):
+    df = kwargs['ti'].xcom_pull(tasks_ids='get_machine_table')
+    hook = MsSqlHook(mssql_conn_id='db_engenharia')
+    target_table_name = 'dbo.fSegmentacao_Simon'
+    merge_query = read_sql_file('sql_files/insert_DBO.FSEGMENTACAO_SIMON.sql')
+
+    for _, row in df.iterrows():
+        params = (kwargs['execution_date'].strftime('%Y-%m-%d'), row['equipamento'], row['segmentacao'], kwargs['execution_date'])
+        hook.get_sqlalchemy_engine().execute(merge_query, params)
 
 with DAG(
     'F_SIMON_SEGMENTACAO',
@@ -37,20 +46,11 @@ with DAG(
         task_id='get_machine_table',
         python_callable=get_machine_registration_table
     )
+    task2 = PythonOperator(
+        task_id='populate_table_fSegmentacao_simon',
+        python_callable=populate_fSegmentacao_simon
+    )
 
 
 
 
-# with DAG(
-#     'example_postgres_dag', 
-#     default_args=default_args, 
-#     schedule='0 7 * * *',
-#     catchup=False
-# ) as dag:
-#     run_query = MsSqlOperator(
-#         task_id='run_sql',
-#         mssql_conn_id='db_engenharia',
-#         sql="SELECT 1;"
-#     )
-
-#     run_query
---
+++ F_SIMON_SEGMENTACAO/sql_files/insert_DBO.FSEGMENTACAO_SIMON.sql
@@ -0,0 +1,9 @@
+MERGE INTO {target_table_name} AS TARGET
+USING (VALUES(?, ?, ?, ?)) AS SOURCE (data, equipamento, segmentacao, updated_at)
+ON target.equipuipamento = source.equipamento AND target.data = source.data
+WHEN MATCHED THEN
+    UPDATE SET target.segmentacao = source.segmentacao
+    UPDATE SET target.updated_at = source.updated_at
+WHEN NOT MATCHED THEN
+    INSERT (data, equipamento, segmentacao, updated_at)
+    VALUES(source.data, source.equipamento, source.segmentacao, source.updated_at)
