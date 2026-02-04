657b425ae121d87eb707edeb4680b1cccf548802
Hudson Alves <hudson.silva@grendene.com.br>
2026-02-04 15:20:24 -0300

    otimiza o arquivo python de disponibilidade para uma funcao apenas

--- deployments/silver/SILVER_F_ELIPSE_DISPONIBILIDADE/SILVER_F_ELIPSE_DISPONIBILIDADE.py
+++ deployments/silver/SILVER_F_ELIPSE_DISPONIBILIDADE/SILVER_F_ELIPSE_DISPONIBILIDADE.py
@@ -1,7 +1,8 @@
 import json
 import logging
 import os
-from datetime import datetime, timedelta
+from datetime import datetime
+from typing import Literal
 
 import pandas as pd
 import psycopg2
@@ -14,9 +15,9 @@ from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.utils.task_group import TaskGroup
-from sqlalchemy import create_engine
-
 from global_modules.functions import getLocalConfig
+from sqlalchemy import create_engine
+from urllib3 import filepost
 
 ENVIROMENT = "PROD"
 
@@ -33,8 +34,18 @@ connection_id_sob = "elipse_sob"
 connection_id_for = "elipse_for"
 connection_id_cra = "elipse_cra"
 
+type Estabelecimento = Literal["sob", "cra", "for"]
+
+
+def get_parquet_path(estab: Estabelecimento):
+    file_path = os.path.join(PASTA_ATUAL, f"extract_data_{str(estab)}.parquet")
+    return file_path
+
+
 database_id = (
-    os.environ.get("DATABASE_DEV") if ENVIROMENT == "DEV" else os.environ.get("DATABASE_PROD")
+    os.environ.get("DATABASE_DEV")
+    if ENVIROMENT == "DEV"
+    else os.environ.get("DATABASE_PROD")
 )
 dw_conn = "postgres_eng_server_dev" if ENVIROMENT == "DEV" else "postgres_eng_server"
 
@@ -60,7 +71,9 @@ def send_teams_message(message: str, webhook_url: str):
     response = requests.post(webhook_url, headers=headers, data=json.dumps(payload))
 
     if response.status_code != 200:
-        raise ValueError(f"Failed to send message: {response.status_code}, {response.text}")
+        raise ValueError(
+            f"Failed to send message: {response.status_code}, {response.text}"
+        )
 
 
 # Função para enviar a mensagem
@@ -95,16 +108,28 @@ default_args = {
     ),
 }
 
+extraction_sql = "extract_paradas.sql"
 
-def extract_data_sob():
-    file_path = os.path.join(PASTA_ATUAL, "extract_data_sob.parquet")
+
+def extract_data(cod_estab: int):
+    match cod_estab:
+        case 20:
+            file_path = get_parquet_path("sob")
+            conn = BaseHook.get_connection(connection_id_sob)
+        case 21:
+            file_path = get_parquet_path("for")
+            conn = BaseHook.get_connection(connection_id_for)
+        case 40:
+            file_path = get_parquet_path("cra")
+            conn = BaseHook.get_connection(connection_id_cra)
+        case _:
+            raise ValueError("Código de estabelecimento inválido")
 
     try:
-        conn = BaseHook.get_connection(connection_id_sob)
         url = f"mssql+pyodbc://{conn.login}:{conn.password}@{conn.host}/{conn.schema}?driver=ODBC+Driver+17+for+SQL+Server"
         hook = create_engine(url)
-        params = (20,)  # id_estabelecimento
-        df = pd.read_sql_query(read_sql_file("extract_paradas.sql"), hook, params=params)
+        params = (cod_estab,)  # id_estabelecimento
+        df = pd.read_sql_query(read_sql_file(extraction_sql), hook, params=params)
         df["ID_Grupo"] = df["ID_Grupo"].astype("Int64")
         df["Cracha_Operador"] = df["Cracha_Operador"].astype("Int64")
         df["Cracha_Preparador"] = df["Cracha_Preparador"].astype("Int64")
@@ -114,65 +139,17 @@ def extract_data_sob():
 
     except Exception as e:
         logging.error(f"FALHA NA CONEXÃO COM O BANCO DE DADOS: {str(e)}")
-        send_teams_message(f"Erro de conexão com o banco de dados: {str(e)}", TEAMS_WEBHOOK_URL)
-
+        send_teams_message(
+            f"Erro de conexão com o banco de dados: {str(e)}", TEAMS_WEBHOOK_URL
+        )
     return file_path
 
 
-def extract_data_for():
-    file_path = os.path.join(PASTA_ATUAL, "extract_data_for.parquet")
-    df = pd.DataFrame()
-
-    try:
-        conn = BaseHook.get_connection(connection_id_for)
-        url = f"mssql+pyodbc://{conn.login}:{conn.password}@{conn.host}/{conn.schema}?driver=ODBC+Driver+17+for+SQL+Server"
-        hook = create_engine(url)
-        params = (21,)  # id_estabelecimento
-        df = pd.read_sql_query(read_sql_file("extract_paradas.sql"), hook, params=params)
-        df["ID_Grupo"] = df["ID_Grupo"].astype("Int64")
-        df["Cracha_Operador"] = df["Cracha_Operador"].astype("Int64")
-        df["Cracha_Preparador"] = df["Cracha_Preparador"].astype("Int64")
-        df["Cracha_Lider"] = df["Cracha_Lider"].astype("Int64")
-        df["Cracha_Apoio"] = df["Cracha_Apoio"].astype("Int64")
-        df.to_parquet(file_path)
-
-    except Exception as e:
-        logging.error(f"FALHA NA CONEXÃO COM O BANCO DE DADOS: {str(e)}")
-        send_teams_message(f"Erro de conexão com o banco de dados: {str(e)}", TEAMS_WEBHOOK_URL)
-
-    return file_path
-
-
-def extract_data_cra():
-    file_path = os.path.join(PASTA_ATUAL, "extract_data_cra.parquet")
-    df = pd.DataFrame()
-
-    try:
-        conn = BaseHook.get_connection(connection_id_cra)
-        url = f"mssql+pyodbc://{conn.login}:{conn.password}@{conn.host}/{conn.schema}?driver=ODBC+Driver+17+for+SQL+Server"
-        hook = create_engine(url)
-        params = (40,)  # id_estabelecimento
-        df = pd.read_sql_query(read_sql_file("extract_paradas.sql"), hook, params=params)
-        df["ID_Grupo"] = df["ID_Grupo"].astype("Int64")
-        df["Cracha_Operador"] = df["Cracha_Operador"].astype("Int64")
-        df["Cracha_Preparador"] = df["Cracha_Preparador"].astype("Int64")
-        df["Cracha_Lider"] = df["Cracha_Lider"].astype("Int64")
-        df["Cracha_Apoio"] = df["Cracha_Apoio"].astype("Int64")
-        df.to_parquet(file_path)
-
-    except Exception as e:
-        logging.error(f"FALHA NA CONEXÃO COM O BANCO DE DADOS: {str(e)}")
-        send_teams_message(f"Erro de conexão com o banco de dados: {str(e)}", TEAMS_WEBHOOK_URL)
-
-    return file_path
-
-
-def concat_dataframes(**kwargs):
-    ti = kwargs["ti"]
+def concat_dataframes():
     file_path = os.path.join(PASTA_ATUAL, "concat_dataframes.parquet")
-    file_sob = ti.xcom_pull(task_ids="extract_all.extract_data_sob")
-    file_cra = ti.xcom_pull(task_ids="extract_all.extract_data_cra")
-    file_for = ti.xcom_pull(task_ids="extract_all.extract_data_for")
+    file_sob = get_parquet_path("sob")
+    file_for = get_parquet_path("for")
+    file_cra = get_parquet_path("cra")
 
     df_sob = pd.read_parquet(file_sob)
     df_for = pd.read_parquet(file_for)
@@ -233,11 +210,18 @@ with DAG(
     tags=["elipse", "disponibilidade", "silver"],
 ) as dag:
     with TaskGroup("extract_all") as extraction:
-        extract_sob = PythonOperator(task_id="extract_data_sob", python_callable=extract_data_sob)
-        extract_for = PythonOperator(task_id="extract_data_for", python_callable=extract_data_for)
-        extract_cra = PythonOperator(task_id="extract_data_cra", python_callable=extract_data_cra)
-
-    concat = PythonOperator(task_id="concat_dataframes", python_callable=concat_dataframes)
+        extract_sob = PythonOperator(
+            task_id="extract_data_sob", python_callable=extract_data, op_args=[20]
+        )
+        extract_for = PythonOperator(
+            task_id="extract_data_for", python_callable=extract_data, op_args=[21]
+        )
+        extract_cra = PythonOperator(
+            task_id="extract_data_cra", python_callable=extract_data, op_args=[40]
+        )
+    concat = PythonOperator(
+        task_id="concat_dataframes", python_callable=concat_dataframes
+    )
 
     load = PythonOperator(task_id="load_stage", python_callable=load_stage)
 
