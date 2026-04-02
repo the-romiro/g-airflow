171e1073e55d2572fb945f44008796806c97b385
Antonio Albuquerque Loiola <romiro@hotmail.com.br>
2026-04-02 16:06:32 -0300

    Centraliza criação de elipse_conn database.py e cria global_module/utils.py.

--- global_modules/database.py
+++ global_modules/database.py
@@ -1,9 +1,36 @@
+from typing import Literal
+
 from airflow.models import Variable
 from sqlalchemy import create_engine, text
 
 from global_modules.sharepoint.logs import log_message
 
 ENG_DATABASE_URL = Variable.get("ENG_DATABASE_URL", None)
+
+type Estabelecimento = Literal["sob", "cra", "for"]
+
+CONNECTIONS_VAR: dict[Estabelecimento, str] = {
+    "sob": "elipse_sob",
+    "for": "elipse_for",
+    "cra": "elipse_cra",
+}
+ESTAB_CODE: dict[Estabelecimento, int] = {
+    "sob": 20,
+    "for": 21,
+    "cra": 40,
+}
+
+
+def get_estab_code(estab: Estabelecimento):
+    return ESTAB_CODE[estab]
+
+
+def get_elipse_conn(estab: Estabelecimento, fast_executemany=False):
+    string_conn = Variable.get(CONNECTIONS_VAR[estab], None)
+    if string_conn is None:
+        raise ValueError(f"Variável do airflow '{CONNECTIONS_VAR[estab]}' não foi definida.")
+
+    return create_engine(string_conn, fast_executemany=fast_executemany)
 
 
 def get_eng_conn(fast_executemany=False):
---
+++ global_modules/utils.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+
+def get_parquet_file(sufixo: str, root_path: str):
+    """
+    Facilita o criação de arquivos `.parquet`.
+
+    :param sufixo: Sufixo para compor o final do nome do arquivo.
+    :type sufixo: str
+    :param root_path: Caminho da pasta para colocar o arquivo. Se for um arquivo, será considerado a pasta deste.
+    :type root_path: str
+    :return: `Path` que representa o arquivo `.parquet`
+    :rtype: Path
+    """
+    f = Path(root_path)
+    root_dir = f.parent if f.is_file() else f
+    file = root_dir.joinpath(f"extract_data_{str(sufixo)}.parquet")
+    return file
+
+
+# reads the sql file and returns the query
+def read_sql_file(filename: str, root_path: str):
+    """
+    Recupera o conteúdo de um arquivo que está em uma pasta `sql_files`.
+
+    :param filename: Nome do arquivo `sql`.
+    :type filename: str
+    :param root_path: Caminho da pasta onde a pasta `sql_files` está. Se for um arquivo, será considerado a pasta deste.
+    :type root_path: str
+    :return: O conteúdo do arquivo.
+    :rtype: str
+    """
+    f = Path(root_path)
+    root_dir = f.parent if f.is_file() else f
+    sql_file = root_dir.joinpath("sql_files", filename)
+
+    if not sql_file.exists():
+        raise ValueError(f"O arquivo '{sql_file.resolve()}' não existe.")
+
+    return sql_file.read_text()
