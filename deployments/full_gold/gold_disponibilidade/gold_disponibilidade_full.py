a2e0d5ca4f11c990e1ee804de376fe55f15f7fb4
Antonio Albuquerque Loiola <romiro@hotmail.com.br>
2026-04-07 15:24:48 -0300

    Refatora gold_disponibilidade_full.

--- deployments/full_gold/gold_disponibilidade/gold_disponibilidade_full.py
+++ deployments/full_gold/gold_disponibilidade/gold_disponibilidade_full.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 
 from airflow import DAG
@@ -5,20 +6,16 @@ from airflow.datasets import Dataset
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
 from global_modules.ms_teams import notify_teams_on_failure
+from global_modules.utils import read_sql_file
 
-# Ambiente
-server = "prod"
-DB = "elipsedev" if server == "dev" else "elipse"
-DW_CONN = "postgres_eng_server_dev" if server == "dev" else "postgres_eng_server"
+DB = os.environ.get("DATABASE_PROD")
+DW_CONN = "postgres_eng_server"
+
+GOLD_TABLE = "gold.oee_fparadas"
 
 # Adicionar o range da carga
 data_inicio = "2024-10-01 05:24:00"
 data_fim = "2025-01-01 06:00:00"
-
-# Nome da conexão definida no Airflow Connections
-connection_id_sob = "elipse_sob"
-connection_id_for = "elipse_for"
-connection_id_cra = "elipse_cra"
 
 
 default_args = {
@@ -35,22 +32,24 @@ with DAG(
     schedule=[gold_dataset],
     catchup=False,
     max_active_runs=1,
+    concurrency=4,
+    tags=["elipse", "gold", "disponibilidade"],
 ) as dag:
     transform_data = SQLExecuteQueryOperator(
         task_id="transform_silver_into_gold",
         conn_id=DW_CONN,
-        sql="./sql_files/transform_query_full.sql",
+        sql=read_sql_file("transform_query_full.sql", __file__),
         params={"hora_inicio": data_inicio, "hora_fim": data_fim, "database": DB},
     )
     vacuum_task = SQLExecuteQueryOperator(
         task_id="vacuum_task",
-        sql=f"VACUUM {DB}.gold.oee_fparadas;",
+        sql=f"VACUUM {DB}.{GOLD_TABLE};",
         conn_id=DW_CONN,
         autocommit=True,  # Isso desabilita a transação para permitir o VACUUM
     )
     analyze_task = SQLExecuteQueryOperator(
         task_id="analyze_task",
-        sql=f"ANALYZE {DB}.gold.oee_fparadas;",
+        sql=f"ANALYZE {DB}.{GOLD_TABLE};",
         conn_id=DW_CONN,
         autocommit=True,
     )
--- deployments/gold/GOLD_F_ELIPSE_DISPONIBILIDADE/main.py
+++ deployments/gold/GOLD_F_ELIPSE_DISPONIBILIDADE/main.py
@@ -1,43 +1,55 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
-from airflow import DAG
 from airflow.datasets import Dataset
+from airflow.decorators import dag
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
 from global_modules.ms_teams import notify_teams_on_failure
+from global_modules.utils import read_sql_file
+
+gold_dataset = Dataset("elipse://gold/f_disponibilidade_simon")
 
 default_args = {
     "owner": "yan arcanjo",
     "start_date": datetime(2025, 1, 26, 6, 5),
     "on_failure_callback": notify_teams_on_failure,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=2),
+    "retry_exponential_backoff": True,
+    "max_retry_delay": timedelta(minutes=30),
 }
 
-gold_dataset = Dataset("elipse://gold/f_disponibilidade_simon")
 
-with DAG(
-    "GOLD_F_DISPONIBILIDADE_SIMON",
+@dag(
+    dag_id="GOLD_F_DISPONIBILIDADE_SIMON",
     default_args=default_args,
     schedule=[gold_dataset],
     catchup=False,
-    tags=["elipse", "self_service", "disponibilidade", "gold"],
     max_active_runs=1,
-) as dag:
-    transform_data = SQLExecuteQueryOperator(
-        task_id="transform_silver_into_gold",
-        conn_id="postgres_eng_server",
-        sql="./sql_files/transform_query.sql",
-    )
-    vacuum_task = SQLExecuteQueryOperator(
+    tags=["elipse", "self_service", "disponibilidade", "gold"],
+)
+def dag_factory():
+    vacuum = SQLExecuteQueryOperator(
         task_id="vacuum_task",
         sql="VACUUM elipse.gold.oee_fparadas;",
         conn_id="postgres_eng_server",
-        autocommit=True,  # Isso desabilita a transação para permitir o VACUUM
+        autocommit=True,
     )
-    analyze_task = SQLExecuteQueryOperator(
+
+    analyze = SQLExecuteQueryOperator(
         task_id="analyze_task",
         sql="ANALYZE elipse.gold.oee_fparadas;",
         conn_id="postgres_eng_server",
         autocommit=True,
     )
 
-_ = vacuum_task >> analyze_task >> transform_data
+    transform = SQLExecuteQueryOperator(
+        task_id="transform_silver_into_gold",
+        conn_id="postgres_eng_server",
+        sql=read_sql_file("transform_query.sql", __file__),
+    )
+
+    _ = vacuum >> analyze >> transform
+
+
+dag_factory()
