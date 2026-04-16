5765735294c2870d85bfc47755ffe18943a6912a
Antonio Albuquerque Loiola <romiro@hotmail.com.br>
2026-04-16 07:54:26 -0300

    Altera wait_dependencies.sql para usar 'start_date' inves de 'execution_date'.

--- global_files/sql_files/wait_dependencies.sql
+++ global_files/sql_files/wait_dependencies.sql
@@ -3,9 +3,10 @@
     GREATEST(COUNT(state), 0),
     '{{ dag }}' AS dag_id,
     (CASE WHEN GREATEST(COUNT(end_date)) = 0 THEN NULL ELSE COUNT(end_date) END) AS end_date
-  FROM dag_run WHERE
+  FROM dag_run
+  WHERE
     1 = 1
-    AND execution_date BETWEEN CURRENT_TIMESTAMP - INTERVAL '{{ params.execute_timedelta['execution_delta'][loop.index - 1] }} MINUTES' AND CURRENT_TIMESTAMP
+    AND start_date BETWEEN CURRENT_TIMESTAMP - INTERVAL '{{ params.execute_timedelta['execution_delta'][loop.index - 1] }} MINUTES' AND CURRENT_TIMESTAMP
     AND dag_id= '{{ dag }}'
     AND state='success'
     -- AND run_type= 'scheduled'
